74571ff79c602db021f7b9c32e196b9c96e8fc2b
nutterrocker <nutter@admin.in.th>
2018-03-16 22:23:54 +0700

    Update scroll-animate.js

--- scroll-animate.js
+++ scroll-animate.js
@@ -5,38 +5,58 @@ let isEnable = false;
 
 /**
  * scrollPositionList 2d ArrayList
- * key: 0 : object dom
- *      1 : scroll position
- *      2 : callback classes
+ * scrollList[index][expectedValue]
+ * 
+ * expectedValue info
+ * 0 for object dom
+ * 1 for scrolling position
+ * 2 callback classes
  */
 let scrollList = [];
 let elementIndex = 0;
 
 /**
- * Collecting elements 
+ * Control function
  */
-$( '.scroll-animate' ).each( ( i,obj ) => {
-    nextClass = $( obj ).attr('class').split('scroll-animate');
-
-    $( obj ).removeClass(nextClass[1]);
-    scrollList.push( [ obj, $( obj ).offset().top, nextClass[1] ] );
-});
-
 function scrollInit() {
     isEnable = true;
 }
+
 function scrollDisable() {
     isEnable = false;
 }
 
+/**
+ * Scroll checking
+ * detect user scrolling
+ */
 function scrollCheck() {
     if( elementIndex < scrollList.length-1 && isEnable ) {
         if( $( window ).scrollTop() >= scrollList[elementIndex][1] ) {
             // Add class
+            $( scrollList[elementIndex][0] ).prop( 'style', null );
             $( scrollList[elementIndex][0] ).addClass( 'animated ' + scrollList[elementIndex][2] );
+
+            // iterate elementIndex
             elementIndex++;
         }
     }
 }
 
-$( window ).scroll( () => { scrollCheck() } );
+/**
+ * Collecting elements 
+ * collect user animation then remove it!
+ */
+$( '.scroll-animate' ).each( ( i,obj ) => {
+    nextClass = $( obj ).attr( 'class' ).split( 'scroll-animate' );
+
+    $( obj ).removeClass( nextClass[1] );
+    $( obj ).attr( 'style', 'visibility: hidden;' );
+
+    scrollList.push( [ obj, $( obj ).offset().top, nextClass[1] ] );
+});
+
+// OnScroll
+$( window ).scroll( () => { 
+    scrollCheck() 
+});
